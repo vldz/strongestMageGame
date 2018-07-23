4ccc03748ae4bdbf6500aba0b398a9468f82920e
vld <vladzelinskyyvlad@gmail.com>
2018-07-23 14:14:09 +0300

    added image 'ready!' added ready for each player, restart for each player

--- strongest mage/ViewController.swift
+++ strongest mage/ViewController.swift
@@ -8,74 +8,93 @@
 
 import UIKit
 
-    class ViewController: UIViewController {
-
+class ViewController: UIViewController {
+    
     @IBOutlet weak var circle: UIImageView!
     @IBOutlet weak var buttonTopStyle: UIButton!
     @IBOutlet weak var bottomMage: UIButton!
     @IBOutlet weak var bottomMageWon: UIImageView!
     @IBOutlet weak var topMageWon: UIImageView!
+    
     @IBOutlet weak var readyButton: UIButton!
-    @IBOutlet weak var reset: UIButton!
-        
+    @IBOutlet weak var readyTop: UIButton!
+    @IBOutlet weak var readyTopPressed: UIImageView!
+    @IBOutlet weak var readyBottomPressed: UIImageView!
+    
+    @IBOutlet weak var restartBottom: UIButton!
+    //@IBOutlet weak var restartTop: UIButton!
+    @IBOutlet weak var restartTop: UIButton!
+    
     var imageYPosition = 0.0
+    var readyCounter = 0
+    var restartCounter = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         view.backgroundColor = UIColor.black
+        
         buttonTopStyle.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
         topMageWon.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
-        self.reset.isHidden = true
+        readyTop.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+        readyTopPressed.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+        restartTop.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+        
         self.circle.center = view.center
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-        
+    
     @IBAction func ready(_ sender: Any) {
-        self.readyF()
+        readyCounter += 1
         readyButton.isHidden = true
+        readyBottomPressed.isHidden = false
+        if readyCounter % 2 == 0 {
+            self.readyF()
         }
-        
-        func readyF() {
-            self.circle.center = view.center
-            imageYPosition = Double(self.circle.center.y)
-            buttonTopStyle.isEnabled = true
-            bottomMage.isEnabled = true
-            circle.isHidden = false
-        }
-        
-    @IBAction func restart(_ sender: Any) {
-        self.resetRestart()
-        readyButton.isHidden = false
     }
-        
-        func resetRestart () {
-            reset.isHidden = true
-            bottomMageWon.isHidden = true
-            topMageWon.isHidden = true
+    
+    @IBAction func readyTop(_ sender: Any) {
+        readyCounter += 1
+        readyTop.isHidden = true
+        readyTopPressed.isHidden = false
+        if readyCounter % 2 == 0 {
+            self.readyF()
         }
-        
+    }
+    
+    func readyF() {
+        self.circle.center = view.center
+        imageYPosition = Double(self.circle.center.y)
+        buttonTopStyle.isEnabled = true
+        bottomMage.isEnabled = true
+        circle.isHidden = false
+        readyTopPressed.isHidden = true
+        readyBottomPressed.isHidden = true
+        readyCounter = 0
+    }
+    
     @IBAction func moveToBottom(_ sender: Any) {
-       UIView.animate(withDuration: 1, delay: 0, options: .curveLinear, animations: {
-        if (self.imageYPosition < Double(UIScreen.main.bounds.height) - Double(self.bottomMage.bounds.height)) {
-            self.circle.center.y += 25
-            self.imageYPosition = Double(self.circle.center.y)
-            print(self.imageYPosition)
-        } else {
-            self.topMageWon.isHidden = false
-            self.bottomMageWon.isHidden = true
-            self.reset.isHidden = false
-            self.buttonTopStyle.isEnabled = false
-            self.bottomMage.isEnabled = false
-            self.circle.isHidden = true
-        }
+        UIView.animate(withDuration: 1, delay: 0, options: .curveLinear, animations: {
+            if (self.imageYPosition < Double(UIScreen.main.bounds.height) - Double(self.bottomMage.bounds.height)) {
+                self.circle.center.y += 25
+                self.imageYPosition = Double(self.circle.center.y)
+                print(self.imageYPosition)
+            } else {
+                self.topMageWon.isHidden = false
+                self.bottomMageWon.isHidden = true
+                self.buttonTopStyle.isEnabled = false
+                self.bottomMage.isEnabled = false
+                self.circle.isHidden = true
+                self.restartBottom.isHidden = false
+                self.restartTop.isHidden = false
+            }
         }, completion: nil)
     }
-       
+    
     @IBAction func moveToTop(_ sender: Any) {
         UIView.animate(withDuration: 1, delay: 0, options: .curveLinear, animations: {
             if (self.imageYPosition > Double(self.buttonTopStyle.bounds.height)) {
@@ -85,12 +104,45 @@ import UIKit
             } else {
                 self.bottomMageWon.isHidden = false
                 self.topMageWon.isHidden = true
-                self.reset.isHidden = false
                 self.buttonTopStyle.isEnabled = false
                 self.bottomMage.isEnabled = false
                 self.circle.isHidden = true
+                self.restartBottom.isHidden = false
+                self.restartTop.isHidden = false
             }
         }, completion: nil)
+    }
+   
+    @IBAction func restartBottomButton(_ sender: Any) {
+        restartCounter += 1
+        self.restartBottom.isHidden = true
+        self.readyBottomPressed.isHidden = false
+            if restartCounter % 2 == 0 {
+            self.restart()
+        }
+    }
+
+    
+    @IBAction func restarttopF(_ sender: Any) {
+        restartCounter += 1
+        self.restartTop.isHidden = true
+        self.readyTopPressed.isHidden = false
+        if restartCounter % 2 == 0 {
+            self.restart()
+        }
+    }
+    
+    func restart() {
+        self.circle.center = view.center
+        imageYPosition = Double(self.circle.center.y)
+        buttonTopStyle.isEnabled = true
+        bottomMage.isEnabled = true
+        circle.isHidden = false
+        self.readyTopPressed.isHidden = true
+        self.readyBottomPressed.isHidden = true
+        bottomMageWon.isHidden = true
+        topMageWon.isHidden = true
+        restartCounter = 0
     }
 }
 
