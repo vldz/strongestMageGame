f6380e9ebde9dc987dca029357f5574212597d38
Dmytrii Ilchuk <dmytriiilchuk@WillKA-Pro.local>
2018-07-25 11:46:49 +0300

    Fixes countdown timers

--- strongest mage/ViewController.swift
+++ strongest mage/ViewController.swift
@@ -30,7 +30,7 @@ class ViewController: UIViewController {
     var readyCounter = 0
     var restartCounter = 0
     var countdownTimer: Timer!
-    var totalTime = 3
+    var totalTime = 4
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,8 +43,8 @@ class ViewController: UIViewController {
         readyTopPressed.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
         restartTop.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
         topTimerImage.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
-        topTimerImage.image = UIImage(named: String(totalTime))
-        bottomTimerImage.image = UIImage(named: String(totalTime))
+        topTimerImage.image = UIImage(named: String(totalTime - 1))
+        bottomTimerImage.image = UIImage(named: String(totalTime - 1))
         topTimerImage.isHidden = true
         bottomTimerImage.isHidden = true
         
@@ -161,17 +161,20 @@ class ViewController: UIViewController {
         readyTopPressed.isHidden = true
         readyBottomPressed.isHidden = true
         readyCounter = 0
-        totalTime = 3
-        topTimerImage.image = UIImage(named: String(totalTime))
-        bottomTimerImage.image = UIImage(named: String(totalTime))
+        totalTime = 4
+        topTimerImage.image = UIImage(named: String(totalTime - 1))
+        bottomTimerImage.image = UIImage(named: String(totalTime - 1))
     }
     
     @objc
     func updateTime() {
-        topTimerImage.image = UIImage(named: String(totalTime))
-        bottomTimerImage.image = UIImage(named: String(totalTime))
-        if totalTime != 0 {
-            totalTime -= 1
+        totalTime -= 1
+        if totalTime > 1 {
+            topTimerImage.image = UIImage(named: String(totalTime - 1))
+            bottomTimerImage.image = UIImage(named: String(totalTime - 1))
+        } else if totalTime == 1 {
+            topTimerImage.image = UIImage(named: "go!")
+            bottomTimerImage.image = UIImage(named: "go!")
         } else {
             endTimer()
         }
