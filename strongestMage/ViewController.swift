8e696efb1ce07548da462a28b52cbdf04aebc279
Dmytrii Ilchuk <dima6615@gmail.com>
2018-08-05 04:39:26 +0300

    Fixes font issues

--- strongestMage/ViewController.swift
+++ strongestMage/ViewController.swift
@@ -61,10 +61,6 @@ class ViewController: UIViewController {
         topTimerImage.isHidden = true
         bottomTimerImage.isHidden = true
         
-        
-        infoLabel.font = UIFont.init(name: "octin vintage b rg", size: 50)
-        let attribute = [NSAttributedStringKey.font: infoLabel.font!]
-        NSMutableAttributedString(string: infoLabel.text!, attributes: attribute)
         infoLabel?.text = "hello"
         
         self.circle.center = CGPoint(x:raySubview.frame.width/2, y:raySubview.frame.height/2)
