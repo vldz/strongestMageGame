1331a5d792dc10e19b191f4d8bc6ae2a722f6801
dmytriiIlchuk <dima6615@gmail.com>
2018-08-20 19:39:13 +0300

    Dmytrii refactoring (#3)
    * Removes an unused ray duplicate
    
    * Renames the ready button for consistency
    
    * Fixes an outlet
    
    * Renames ready button for consistency
    
    * Reworks phrase generator
    
    * Introduces new models for the game scene and for mages' entities
    
    * Changes the events type to touchDown instead of touchUp
    
    * Introduces game states
    
    * Adds some TODOs, continues refactoring
    
    * Moves game logic to GameScene and Mage classes

---
+++ strongestMage/models/Mage.swift
@@ -0,0 +1,85 @@
+//
+//  Mage.swift
+//  strongestMage
+//
+//  Created by Dmytrii Ilchuk on 8/17/18.
+//  Copyright © 2018 vld. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class Mage {
+    enum State {
+        case initial
+        case ready
+        case countdown
+        case active
+        case winner
+        case loser
+        case finished
+    }
+    var state: State! {
+        willSet(newState) {
+            handleStateChange(newState: newState)
+        }
+    }
+    var body: UIButton!
+    var ray: UIImageView!
+    var infoLabel: UILabel!
+    var readyButton: UIButton!
+
+    init(body : UIButton, ray: UIImageView, infoLabel: UILabel, readyButton: UIButton) {
+        self.state = .initial
+        self.body = body
+        self.ray = ray
+        self.infoLabel = infoLabel
+        self.readyButton = readyButton
+    }
+
+    func handleStateChange(newState: State) {
+        switch newState {
+        case .initial:
+            body.isEnabled = false
+            ray.isHidden = true
+            infoLabel.isHidden = true
+            readyButton.setTitle("ready?", for: .normal)
+            readyButton.isEnabled = true
+        case .countdown:
+            infoLabel.isHidden = false
+            readyButton.isHidden = true
+        case .ready:
+            readyButton.setTitle("ready!", for: .normal)
+            readyButton.isEnabled = false
+        case .active:
+            body.isEnabled = true
+        // TODO: use separate ray for each mage
+            infoLabel.isHidden = true
+        case .winner:
+            showInfo(info: PhrasesGenerator.getPhrase(.win))
+            state = .finished
+        case .loser:
+            showInfo(info: PhrasesGenerator.getPhrase(.lose))
+            state = .finished
+        case .finished:
+            body.isEnabled = false
+        // TODO: Handle separate ray
+            readyButton.setTitle("restart?", for: .normal)
+            readyButton.setTitleColor(UIColor(red: 0.00, green: 1.00, blue: 0.82, alpha: 1.0), for: .normal)
+            readyButton.isEnabled = true
+            readyButton.isHidden = false
+        }
+    }
+
+    func showInfo(info: String) {
+        infoLabel.text = info
+        infoLabel.isHidden = false
+    }
+
+    func isReady() -> Bool {
+        return state == .ready
+    }
+
+    func setState(newState: State) {
+        self.state = newState
+    }
+}
